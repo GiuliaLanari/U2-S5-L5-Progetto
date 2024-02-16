44b300e4d7be14759256100d2b813232d2ab31bf
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-16 17:41:50 +0100

    sistemazione icone contenitore

---
+++ base_di_partenza/assets/JavaScript/index.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menù");
+const bottone = document.querySelector(".btn-menu");
+
+window.addEventListener("scroll", function () {
+  document.getElementById("showScroll").innerHTML = window.menu.style.backgraundColor = "white";
+});
