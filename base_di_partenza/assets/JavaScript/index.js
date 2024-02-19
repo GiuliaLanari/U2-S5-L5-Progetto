c2e9bc6cd0782421625f72bfa5e45497ee9fffad
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-19 18:19:27 +0100

    colore nav

--- base_di_partenza/assets/JavaScript/index.js
+++ base_di_partenza/assets/JavaScript/index.js
@@ -1,37 +1,32 @@
-/*
 const menu = document.querySelector(".menù");
-const bottone = document.querySelector(".btn-menù")
-
-window.addEventListener.menu("scroll", cambioColoreMenu () {
-  document.getElementById("showScroll").innerHTML = window.pageY0ffset +"px";
-console.log(showScroll)
-});
-}
-const cambioColoreMenu = ()=> {
-  if (scrollY=== 10px) {
-     menu.style.backgraundColor = "white";
-  bottone.style.backgraundColor = "#1a8917";
-  }
-}
-*/
-
-let posizioneScroll = 0;
-let menu = document.querySelector(".menù");
 const bottone = document.querySelector(".btn-menù");
 
-const cambioColoriMenu = function (scroll) {
-  menu.style.backgraundColor = "white";
-  bottone.style.backgraundColor = "#1a8917";
+window.onscroll = function (e) {
+  console.log(window.scrollY);
+  const scrollY = window.screenY;
 
-  document.addEventListener("scroll", (event) => {
-    posizioneScroll = window.scrollY;
-
-    if (posizioneScroll !== 0) {
-      window.requestAnimationFrame(() => {
-        cambioColoriMenu(posizioneScroll);
-      });
-    }
-  });
+  if (scrollY >= 300) {
+    menu.classList.add("scrolled");
+    bottone.classList.add("scrolled");
+  } else {
+    menu.classList.remove("scrolled");
+    bottone.classList.remove("scrolled");
+  }
 };
+/*
+window.addEventListener("scroll", function () {
+  const menu = document.querySelector(".menù");
+  const bottone = document.querySelector(".btn-menù");
+  const altazzaCambio = document.querySelector(".startReading");
+  const altezzaPunto = height.getBoundingClientRect().top;
+  const menuAltezza = menu.offsetHeight;
 
-cambioColoriMenu();
+  if (altezzaPunto < menuAltezza) {
+    menu.classList.add("scrolled");
+    bottone.classList.add("scrolled");
+  } else {
+    menu.classList.remove("scrolled");
+    bottone.classList.remove("scrolled");
+  }
+});
+*/
