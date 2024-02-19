1bdeac9d3e4ab7dacfee0f14dd628a6b64c8f34a
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-19 09:00:19 +0100

    Prova del cambio colore Menù

--- base_di_partenza/assets/JavaScript/index.js
+++ base_di_partenza/assets/JavaScript/index.js
@@ -1,6 +1,36 @@
+/*
 const menu = document.querySelector(".menù");
-const bottone = document.querySelector(".btn-menu");
+const bottone = document.querySelector(".btn-menù")
 
 window.addEventListener("scroll", function () {
-  document.getElementById("showScroll").innerHTML = window.menu.style.backgraundColor = "white";
+  document.getElementById("showScroll").innerHTML = window.pageY0ffset +"px";
+console.log(showScroll)
 });
+
+
+onscroll= (event)=> {
+
+
+}
+*/
+
+let posizioneScroll = 0;
+let menu = document.querySelector(".menù");
+const bottone = document.querySelector(".btn-menù");
+
+const cambioColoriMenu = function (scroll) {
+  menu.style.backgraundColor = "white";
+  bottone.style.backgraundColor = "#1a8917";
+
+  document.addEventListener("scroll", (event) => {
+    posizioneScroll = window.scrollY;
+
+    if (posizioneScroll !== 0) {
+      window.requestAnimationFrame(() => {
+        cambioColoriMenu(posizioneScroll);
+      });
+    }
+  });
+};
+
+cambioColoriMenu();
